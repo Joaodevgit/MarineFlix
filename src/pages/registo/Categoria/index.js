5a848506fc45f94f45ffd84b63a6d2edeed6717c
Joaodevgit <joaotiagopereira@sapo.pt>
2020-08-02 10:23:02 +0100

    Commit aula 3

--- src/pages/registo/Categoria/index.js
+++ src/pages/registo/Categoria/index.js
@@ -1,22 +1,101 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageDefault from '../../../components/PageDefault';
 import {Link} from 'react-router-dom';
+import FormField from '../../../components/FormField';
 
 
-function AddCategoria() {
+function AddCategory() {
+    const initialValues = {
+        nome:'',
+        descricao:'',
+        cor:'',
+    }
+
+    const [categories, setCategories] = useState([]);
+    const [values, setValues] = useState(initialValues);
+    
+    function setValue(key,value){
+        //key: nome, descrição, cor,...
+        setValues({
+            ...values,
+            [key]: value, // nome: 'value'
+        })
+    }
+
+    function handlerChange(eventInfo) {
+        setValue(
+            eventInfo.target.getAttribute('name'),
+            eventInfo.target.value
+        );
+    }
+
     return ( 
         <PageDefault >
-        <h1> Adição de Categoria</h1>
+        <h1> Adição de Categoria {values.nome} </h1>
 
-        <form>
+        <form onSubmit={function handleSubmit(eventInfo){
+            eventInfo.preventDefault();
+            console.log('Foi feita uma submissão');
+            setCategories([
+                ...categories, 
+                values
+            ]);
+            
+            setValues(initialValues);
+        }}>
+
+        <FormField
+            label ="Nome da Categoria: "
+            type="text"
+            name = "nome"
+            value = {values.nome}
+            onChange = {handlerChange}
+        />
+
+        {/* VER COMO FAZER O TEXTAREA*/}
+        <FormField
+            label ="Descrição da Categoria: "
+            type="???"
+            name = "descricao"
+            value = {values.descricao}  
+            onChange = {handlerChange}
+        />
+
+        {/*
+        <div>
             <label>
-                Nome da Categoria:
-                <input type="text"/>
+                Descrição da Categoria:
+                <textarea type = "text"
+                          value = {values.descricao}
+                          name= "descricao"
+                          onChange = {handlerChange}
+                /> 
             </label>
-            <button>
-               Adicionar
-            </button>
+        </div>
+        */}
+        <FormField
+            label ="Cor: "
+            type="color"
+            name = "cor"
+            value = {values.cor}
+            onChange = {handlerChange}
+        /> 
+            
+        <button>
+            Adicionar
+        </button>
         </form>
+
+
+        <ul>
+            {categories.map((category, index) => {
+                return (
+                    <li key = {`${category} ${index}`}> {/*usado para garantir que a key em li é único*/}
+                        {category.nome}
+                    </li>
+                )
+            })}
+        </ul>
 
         <Link to = "/" >
         Regressar à homepage 
@@ -25,4 +104,4 @@ function AddCategoria() {
     )
 }
 
-export default AddCategoria;
+export default AddCategory;
